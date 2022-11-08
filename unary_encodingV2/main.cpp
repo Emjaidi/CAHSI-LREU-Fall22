cbe8ab9aaddaf808b4ef24b9d510ab052fff6092
emjadi <madelas797@protonmail.ch>
2022-11-08 08:09:59 -0600

    Initial Commit

---
+++ unary_encodingV2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  unary_encodingV2
+//
+//  Created by MJ Asuncion on 11/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
