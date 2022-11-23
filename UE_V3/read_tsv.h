ecb423d6e5e05fa28cf5d1b7a899cc198bbe4fb0
emjadi <madelas797@protonmail.ch>
2022-11-23 10:18:53 -0600

    converted data points to a class for convenience

--- UE_V3/read_tsv.h
+++ UE_V3/read_tsv.h
@@ -12,47 +12,109 @@
 
 #define FOR(i,a,b) for(int i=(a);i<(b);++i)
 #define REP(i,n) FOR(i,0,n)
-#define VECTWODUB std::vector<std::vector<long double> > 
-
+#define VECTWODUB std::vector<std::vector<double> >
 
 #ifndef read_tsv_h
 #define read_tsv_h
 
+/* BEGIN Data_Point class*/
+class Data_Point
+{
+    double latitude = -1.0;
+    double longtitude = -1.0;;
+    int cell = -1;
+    int perturbed_cell = -1;
+public:
+    void print_coord();
+    void set_coord(double, double);
+    void set_cell(double, double);
+    void perturb_cell();
+};
 
-long double max_x, max_y, min_x, min_y; //keeps track of the graph limits
+void Data_Point::set_coord(double x, double y)
+{
+    latitude = x;
+    longtitude = y;
+}
 
-VECTWODUB read_tsv(char* fname) {
+void Data_Point::print_coord()
+{
+    std::cout << "Location: " << std::setprecision(16) << latitude << ", "  << longtitude << std::endl;
+}
+/*END Data_Point class*/
+
+double max_x, max_y, min_x, min_y; //keeps track of the graph limits
+
+std::vector<Data_Point> read_tsv(char* fname) {
     std::ifstream ifs(fname);
-    VECTWODUB items; //Stores the x and y coordinate of row in the tsv
+    std::vector<Data_Point> items; //Stores the x and y coordinate along with the cell and the perturbed cell of row in the tsv
     
     if (ifs.fail()) { //incase stream fails
-        VECTWODUB err(1,std::vector<long double> (1,-1));
+        Data_Point item;
+        std::vector<Data_Point> err(1,item);
         std::cerr << "error" << std::endl;
         return err;
     }
     std::string line;
     while (getline(ifs, line)) { //as long as there is input from line
         std::stringstream ss(line);
-        std::vector<long double> item;//stores the columns in the row
+        Data_Point item;//stores the columns in the row
         std::string tmp;
-        int i = 0;
+        int i = 0; //keeps track of the column index
+        double x {};
+        double y {};
         while(getline(ss, tmp, '\t')) {
-            if(i == 4 || i == 5){
-                item.push_back(stod(tmp));
+            if(i == 4 ){
+                x = stod(tmp);
+            }
+            else if(i == 5 ){
+                y = stod(tmp);
             }
             i++;
         }
+        item.set_coord(x,y);
         items.push_back(item);
     }
     return items;
 }
 
+
+
 //void grap_limit_x(int i)
-void print_v2d(VECTWODUB items){
-    REP(i,items.size()) {
-        REP(j, items[i].size())
-            std::cout << "[" << i << "][" << j <<"]: " << std::setprecision(16) << items[i][j] << std::endl;
-    }
-}
+
+//VECTWODUB read_tsv_2v(char* fname) {
+//    std::ifstream ifs(fname);
+//    VECTWODUB items; //Stores the x and y coordinate of row in the tsv
+//    
+//    if (ifs.fail()) { //incase stream fails
+//        VECTWODUB err(1,std::vector<double> (1,-1));
+//        std::cerr << "error" << std::endl;
+//        return err;
+//    }
+//    std::string line;
+//    while (getline(ifs, line)) { //as long as there is input from line
+//        std::stringstream ss(line);
+//        std::vector<double> item;//stores the columns in the row
+//        std::string tmp;
+//        int i = 0;
+//        while(getline(ss, tmp, '\t')) {
+//            if(i == 4 || i == 5){
+//                item.push_back(stod(tmp));
+//            }
+//            i++;
+//        }
+//        items.push_back(item);
+//    }
+//    return items;
+//}
+
+//void print_v2d(VECTWODUB items){
+//    REP(i,items.size()) {
+//        REP(j, items[i].size())
+//            std::cout << "[" << i << "][" << j <<"]: " << std::setprecision(16) << items[i][j] << std::endl;
+//    }
+//}
+
+
 
 #endif /* read_tsv_h */
--- UE_V3/test.cpp
+++ UE_V3/test.cpp
@@ -3,11 +3,18 @@
 
 #include "read_tsv.h"
 
-//std::vector<std::vector<int> > items; //Stores the x and y coordinate of row in the tsv
-//#define VECTWODUB std::vector<std::vector< long double> > 
+int main()
+{
+    std::vector<Data_Point> coordinates = read_tsv((char *)"test.txt");
+    REP(i, coordinates.size())
+    {
+        std::cout << "User: " << i;
+        std::cout << "\t";
+        coordinates.at(i).print_coord();
+        std::cout << std::endl;
+    }
+    //print_v2d(coordinates);
+    //std::cout << 'g';
 
-int main(){
-    VECTWODUB coordinates = read_tsv((char *)"test.txt");
-    print_v2d(coordinates);
     return 0;
 }
